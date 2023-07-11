79c090d609b54e8eaf33ace019a3cdff608ff5ae
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-11 22:42:27 +0300

    Add files via upload

---
+++ Update.sql
@@ -0,0 +1,5 @@
+UPDATE person set [Name] = 'Bob' WHERE [Name] = 3
+UPDATE person set [Name] = 'Vincent' WHERE [DateBirth] > 1997
+UPDATE person set [Name] = 'Alla' WHERE [Name] = 'Alex' OR [DateBirth] = 2002
+
+
