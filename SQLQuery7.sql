0640070ec08c0c7aa4d324ef44441864cf3665df
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-11 22:54:07 +0300

    Add files via upload

---
+++ SQLQuery7.sql
@@ -0,0 +1,93 @@
+USE [HOMEWORK]
+GO
+
+INSERT INTO [dbo].[person]
+           ([Name]
+           ,[PhoneNumber]
+           ,[EmailAddress]
+           ,[DateBirth])
+     VALUES
+           ('Chris',
+           900-2332-123,
+           'chris@mail.com',
+           2001)
+
+
+INSERT INTO [dbo].[addres]
+           ([Street]
+           ,[City]
+           ,[Country])
+     VALUES
+           ('Main',
+           'London',
+           'England')
+
+
+INSERT INTO [dbo].[info]
+           ([StudentNumber]
+           ,[AverageMark])
+     VALUES
+           (32,
+           4.3)
+
+
+
+INSERT INTO [dbo].[person]
+           ([Name]
+           ,[PhoneNumber]
+           ,[EmailAddress]
+           ,[DateBirth])
+     VALUES
+           ('John',
+           901-2542-543,
+           'john@mail.com',
+           2002)
+
+
+INSERT INTO [dbo].[addres]
+           ([Street]
+           ,[City]
+           ,[Country])
+     VALUES
+           ('Bottom',
+           'London',
+           'England')
+
+
+INSERT INTO [dbo].[info]
+           ([StudentNumber]
+           ,[AverageMark])
+     VALUES
+           (12,
+           3.8)
+
+
+
+INSERT INTO [dbo].[person]
+           ([Name]
+           ,[PhoneNumber]
+           ,[EmailAddress]
+           ,[DateBirth])
+     VALUES
+           ('Alex',
+           900-1232-143,
+           'alex@mail.com',
+           2001)
+
+
+INSERT INTO [dbo].[addres]
+           ([Street]
+           ,[City]
+           ,[Country])
+     VALUES
+           ('Avenu',
+           'London',
+           'England')
+
+
+INSERT INTO [dbo].[info]
+           ([StudentNumber]
+           ,[AverageMark])
+     VALUES
+           (21,
+           4.1)
